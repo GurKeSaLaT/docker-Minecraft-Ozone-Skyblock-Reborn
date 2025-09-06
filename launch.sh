badd20984ac96c1233b2a497d424ff2f2237f6f3
GurKeSaLaT <jonas.lukitsch@googlemail.com>
2025-09-06 22:36:01 +0200

    added move modpack from subdir in main dir

--- launch.sh
+++ launch.sh
@@ -17,6 +17,13 @@ if ! [[ -f Ozone-Skyblock-Reborn_Server_1.19.zip ]]; then
 	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu Ozone-Skyblock-Reborn_Server_*.zip
 	curl -Lo Ozone-Skyblock-Reborn_Server_1.19.zip 'https://edge.forgecdn.net/files/6917/151/OSR%20Server%20-%201.19.zip' 
 	unzip -u -o Ozone-Skyblock-Reborn_Server_1.19.zip -d /data
+	if [[ $(find /data -maxdepth 2 -name 'mods' -type d | wc -c) -gt 11 ]]; then
+		INSTALL_SUBDIR=$(find /data -maxdepth 2 -name 'mods' -type d | sed 's/\/mods//')
+		mv -f $(echo $INSTALL_SUBDIR)/* /data
+		rm -fr $(echo $INSTALL_SUBDIR)
+	fi
+
+
 	curl -Lo forge-${FORGE_VERSION}-installer.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/forge-$FORGE_VERSION-installer.jar
 	java -jar forge-${FORGE_VERSION}-installer.jar --installServer
 fi
